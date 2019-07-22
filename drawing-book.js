d1f881a09d773ceae19001cef9d73ea9e7a85a6d
vieiramanda11 <amanda_vieira@outlook.com>
2019-07-22 19:06:49 -0300

    adding drawing book

---
+++ drawing-book.js
@@ -0,0 +1,10 @@
+function pageCount(n, p) {
+    let frontPage = Math.floor(p / 2); 
+    let backPage = Math.floor((n / 2) - frontPage);
+    
+    if (frontPage < backPage) {
+        return frontPage;
+    } else {
+        return backPage;
+    }
+}
